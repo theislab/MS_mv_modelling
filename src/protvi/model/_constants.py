4e8778fa992327e392aeaeca4a8ffa1231a21ab7
soroorh <soroorh@student.unimelb.edu.au>
2024-06-28 08:48:49 +0200

    update _constants

--- src/protvi/model/_constants.py
+++ src/protvi/model/_constants.py
@@ -5,6 +5,7 @@ class _EXTRA_KEYS_NT(NamedTuple):
     PRIOR_CAT_COVS_KEY: str = "prior_categorical_covs"
     PRIOR_CONT_COVS_KEY: str = "prior_continuous_covs"
     NORM_CONT_COVS_KEY: str = "norm_continuous_covs"
+    TREND_BATCH_KEY: str = "detection_trend_key"
 
 
 EXTRA_KEYS = _EXTRA_KEYS_NT()
