241884e8605fc0fd3ea5feb51c3e089d8c867eec
RasmusML <rasmusml@hotmail.dk>
2024-08-26 13:55:07 +0200

    Move requirements into pyproject + use sphinx book theme

--- docs/conf.py
+++ docs/conf.py
@@ -36,5 +36,17 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+
+html_title = project
+
+html_theme_options = {
+    # "repository_url": repository_url,
+    # "use_repository_button": True,
+    "path_to_docs": "docs/",
+    "navigation_with_keys": False,
+}
+
+pygments_style = "default"
